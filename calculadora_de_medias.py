7718e5a1bac7bfce7da989cf61a58b2ce3d164f4
igorgmnz <igorgmnzmonte@gmail.com>
2025-03-26 15:33:14 -0300

    atualizando código

--- calculadora_de_medias.py
+++ calculadora_de_medias.py
@@ -2,8 +2,10 @@ print("Bem vindo à calculadora de médias")
 
 notas = []
 quantos_numeros = int(input("quantas notas deseja adicionar?: "))
-for i in range (quantos_numeros):
+for notes in range (quantos_numeros):
     notas.append(int(input("insira aqui sua nota: ")))
 soma = sum(notas)
 media = soma / len(notas)
-print("A média das suas notas é:", media)
+
+print('Cálculo a ser feito: {}/{}'.format(soma, len(notas)))
+print("A média das suas notas é: {:.1f}".format(media))
