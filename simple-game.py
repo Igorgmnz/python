142cf6f829cd8d4b52e6d46147dc85adf43eb213
igorgmnz <igorgmnzmonte@gmail.com>
2025-04-08 17:05:15 -0300

    somente algumas atualizações

--- simple-game.py
+++ simple-game.py
@@ -1,16 +1,20 @@
-while True:
+from random import randint
+from time import sleep
 
-    from random import randint
 
-    numero_sorteado = randint(1, 3)
 
-    print("bem vindo ao jogo de adivinhar!")
-    print("escolha um numero de 1 a 3 e teste sua intuição!")
-    print("(1) (2) (3)")
-    valor_usuario = int(input("insira aqui seu número: "))
-    if valor_usuario > 3:
-        print("insira um valor entre 1 e 3 jumento!!!")
-        break
-    if valor_usuario == numero_sorteado:
-        print("Parabéns por nada!!! ;)")
-    else: print(f"Errou feio, {numero_sorteado} era o certo rs")
+numero_sorteado = randint(1, 3)
+
+print("bem vindo ao jogo de adivinhar!")
+print("escolha um numero de 1 à 3 e teste sua intuição!")
+print('\033[7;30;40m-=-\033[m' * 20)
+print('\033[3;32;40m                      (1) (2) (3)                           \033[m')
+print('\033[7;30;40m-=-\033[m' * 20)
+valor_usuario = int(input("insira aqui seu número: "))
+print('\033[31mPROCESSANDO...\033[m')
+sleep(3)
+if valor_usuario > 3:
+    print("insira um valor entre 1 e 3 jumento!!!")
+if valor_usuario == numero_sorteado:
+    print(f"Parabéns, realmente era \033[32m{valor_usuario}\033[m!")
+else: print(f"Errou feio, \033[31m{numero_sorteado}\033[m era o número certo rs")
